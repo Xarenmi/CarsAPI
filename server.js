e1d383786946753d841295598f94c9f63d9ccec6
1106 <sanders.gesture-0c@icloud.com>
2024-04-15 20:24:17 -0600

    Database connection config

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+import { connect } from './config/database.js'
 import express from 'express'
 
 const PORT = process.env.PORT || 3000
 
+connect()
+
 const api = express()
 api.use(express.json())
 
+// Aqui van las rutas
+
 api.listen(PORT, () => {
-  console.log(`Sever is running on http://localhost:${PORT}`)
+  console.log(`Sever is running on http://localhost:${PORT} 🚀`)
 })
