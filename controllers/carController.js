310674090b0b8426850710bf3b3a48f1db8bb987
1106 <sanders.gesture-0c@icloud.com>
2024-04-17 19:12:40 -0600

    updateByCarId method created

--- controllers/carController.js
+++ controllers/carController.js
@@ -33,11 +33,21 @@ const getCarById = async (req, res) => {
 }
 
 // Update
+const updateByCarById = async (req, res) => {
+  if (Object.keys(req.body).length === 0) return res.status(400).json({ msg: 'No data to update' })
+  try {
+    const updateCar = await Car.findByIdAndUpdate(req.params.carId, req.body, { new: true })
+    res.status(200).json(updateCar)
+  } catch (error) {
+    res.status(400).json({ msg: 'Error updating car', error })
+  }
+}
 
 // Delete
 
 export {
   createCar,
   getCars,
-  getCarById
+  getCarById,
+  updateByCarById
 }
--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -7,5 +7,6 @@ const carRoutes = express.Router()
 carRoutes.post('/cars', carController.createCar)
 carRoutes.get('/cars', carController.getCars)
 carRoutes.get('/cars/:carId', carController.getCarById)
+carRoutes.patch('/cars/:carId', carController.updateByCarById)
 
 export default carRoutes
